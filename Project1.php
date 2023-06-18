4c048c9dbfebddce1f521ac4ad0c2d89da5b4eea
a <115686094+lebronjamesonthecomputer@users.noreply.github.com>
2023-06-18 01:18:16 -0400

    Add files via upload

--- Project1.php
+++ Project1.php
@@ -1,24 +1,40 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="UTF-8">
+	<title>GGC Fashion Club</title>
+	<link rel="stylesheet" href="normalize.css">
+	<link rel="stylesheet" href="style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300italic,300' rel='stylesheet' type='text/css'>
+	<script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+	<script src="main.js"></script>
+	<script src="https://www.w3schools.com/lib/w3.js"></script>
     <title>Project1</title>
     </head>
     <body>
+    
     <?php
-        include "Lab2functions.php";
-
-        greeting($_POST["myName"]);
         
-        echo "Thank you for taking the student survey.<br><br>";
+        echo "<video autoplay muted loop id=\"myVideo\">";
+        echo "<source src=\"main.mp4\" type=\"video/mp4\">";
+        
+        echo "</video>";
+        echo "<div class = \"center-screen\">";
+        echo "<h1>Thank you for completing the registration.</h1>";
+       
+        echo "<div class = \"wrp\">";
+        echo "<p>";
         echo "Email: " . $_POST["email"] . "<br>";
         echo "Phone: " . $_POST["phone"] . "<br>";
+        echo "Birthday: " . $_POST["luckyDay"] . "<br><br>";
         echo "Gender: " . $_POST["gender"] . "<br>";
         echo "level: " . $_POST["level"] . "<br><br>";
-
+    
         echo "Your answers for the ITEC knowledge quiz:<br>";
         echo "Q1: ". $_POST["Q1"] . "<br>";
         echo "Q2: ". $_POST["Q2"] . "<br>";
-        echo "Q3: ";
+        echo "Q3:";
         if ($_POST["Q3A"] == "yes")
             echo "A ";
         if ($_POST["Q3B"] == "yes")
@@ -27,18 +43,26 @@
             echo "C ";
         if ($_POST["Q3D"] == "yes")
             echo "D ";
-        echo "<br><br>";
+        echo "<br>";
+        echo "Q4: ". $_POST["Q4"] . "<br>";
+        echo "<br>";
 
-        echo "Your quiz score<br>";
         $score = 0;
-        if ($_POST["Q1"] == "C")
+        if ($_POST["Q1"] == "D")
             $score += 50;
-        if ($_POST["Q2"] == "F")
+        if ($_POST["Q2"] == "A")
             $score += 50;
-        if (($_POST["Q3A"] == "yes") && ($_POST["Q3B"] == "yes") && ($_POST["Q3C"] == "yes") && ($_POST["Q3D"] != "yes"))
+        if (($_POST["Q3A"] == "yes") && ($_POST["Q3B"] == "yes") && ($_POST["Q3C"] != "yes") && ($_POST["Q3D"] == "yes"))
             $score += 50;
-        echo "<h3 style = 'color:red;'>" . $score . "</h3>";
+         if ($_POST["Q4"] == "B")
+            $score += 50;
+        echo "Your quiz score: ";
+
+        echo "<br><span style = 'color:red;'><b>" . $score . "</b></span></p>";
+
+        echo "</div>";
+        echo "</div>";
+
     ?>
     </body>
 </html>
-
---
+++ main.js
@@ -0,0 +1,121 @@
+$(document).ready(function(){
+	$(".image").width($("#slider").width()/3.3);
+	$(".item-img-1").addClass("item-img-loaded")
+
+	var loaded = true;
+
+	function reset(){
+		$(".image").width($("#slider").width()/3.3);
+		$(".image").attr("data-click",0);
+
+		$(".image").find(".item-img-2").css({
+			opacity:0
+		})
+
+		$(".image").find(".item-img-1").css({
+			opacity:1
+		})
+	}
+
+	reset();
+
+	$(window).resize(function(){
+		reset();
+	})
+
+	$(".item-img-1, .item-img-2").each(function(){
+		var src = $(this).attr("data-src")
+		$(this).css({
+			background:`url(images/${src}) no-repeat`,
+		})
+	})
+
+	$(".image").mouseover(function(){
+		if ($(window).width() > 980) {
+			if (loaded) {
+				$(".image:first-child").mouseout();
+			}
+			$(this).find(".item-img-1").css({
+				opacity:0
+			})
+
+			$(this).find(".item-img-2").css({
+				opacity:1,
+			})
+
+			$(this).find(".image-text").css({
+				opacity:1
+			})
+			$(this).find(".image-question").css({
+				opacity:1
+			})
+
+			$(".image").width($("#slider").width()/4)
+			$(this).width($("#slider").width()/2)
+		}
+	})
+
+	$(".image").mouseout(function(){
+		if ($(window).width() > 980) {
+			$(this).find(".item-img-2").css({
+				opacity:0
+			})
+
+			$(this).find(".item-img-1").css({
+				opacity:1
+			})
+
+			$(".image").width($("#slider").width()/3.3)
+		}
+	})
+
+	$(".image:last-child").mouseover(function(event){
+		if ($(window).width() > 980) {
+			$(this).find(".item-img-1").css({
+				opacity:0
+			})
+	 
+			$(this).find(".item-img-2").css({
+				opacity:1
+			})
+
+			$(".image").width($("#slider").width()/5)
+			$(this).width($("#slider").width()/1.5)
+		}
+	})
+
+	$(".image").click(function(event){
+		event.stopPropagation();
+
+
+		if ($(window).width() <= 980) {
+			if ($(window).width() > 980) {
+			if (loaded) {
+				$(".image:first-child").mouseout();
+			}
+			$(this).find(".item-img-1").css({
+				opacity:0
+			})
+
+			$(this).find(".item-img-2").css({
+				opacity:1,
+			})
+
+			$(this).find(".image-text").css({
+				opacity:1
+			})
+
+			$(".image").width($("#slider").width()/4)
+			$(this).width($("#slider").width()/2)
+		}
+		}
+	})
+
+	// note to remove
+	$(".image:first-child").mouseover();
+
+	$(window).click(function(){
+		reset();
+	})
+})
+
